540a17dd2a6249b20e23fb4c1481bd8d8b5770c9
ge28boj <ge28boj@mytum.de>
2024-01-03 12:11:59 +0100

    second try

--- rv32gc-toolchain.cmake
+++ rv32gc-toolchain.cmake
@@ -22,7 +22,7 @@ set(CMAKE_CXX_COMPILER "${RISCV_TOOLCHAIN}-g++${EXE_EXT}")
 
 
 
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -pg -D__riscv__ -march=${RISCV_ARCH} -mabi=${RISCV_ABI}")
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -D__riscv__ -march=${RISCV_ARCH} -mabi=${RISCV_ABI}")
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -D__riscv__ -march=${RISCV_ARCH} -mabi=${RISCV_ABI}")
 set(CMAKE_ASM_FLAGS "${CMAKE_ASM_FLAGS} -D__riscv__ -march=${RISCV_ARCH} -mabi=${RISCV_ABI}")
 set(CMAKE_EXE_LINKER_FLAGS "-march=${RISCV_ARCH} -mabi=${RISCV_ABI}")
